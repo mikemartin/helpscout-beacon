22b6c3d61eb1e275019534c23f6de21ba4295d08
Mike Martin <hello@mike-martin.ca>
2026-03-10 00:30:41 +1100

    Fix beacon initialization

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -1,17 +1,19 @@
-let helpscout = Statamic.$config.get('helpscout');
+Statamic.booting(() => {
+  let helpscout = Statamic.$config.get('helpscout');
 
-if (helpscout && helpscout.beacon_id) {
-  if (helpscout.avatar) {
-    helpscout.user['avatar'] = helpscout.user['website'] + helpscout.avatar;
+  if (helpscout && helpscout.beacon_id) {
+    if (helpscout.avatar) {
+      helpscout.user['avatar'] = helpscout.user['website'] + helpscout.avatar;
+    }
+
+    let script = document.createElement('script');
+    script.src = helpscout.beacon_script_url;
+
+    script.addEventListener('load', () => {
+      window.Beacon('init', helpscout.beacon_id);
+      window.Beacon('identify', helpscout.user);
+    });
+
+    document.body.appendChild(script);
   }
-
-  let script = document.createElement('script');
-  script.src = helpscout.beacon_script_url;
-
-  script.addEventListener('load', () => {
-    window.Beacon('init', helpscout.beacon_id);
-    window.Beacon('identify', helpscout.user);
-  });
-
-  document.body.appendChild(script);
-}
+});
